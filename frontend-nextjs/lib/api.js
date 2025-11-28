6406fe5b206cef81613113feb7cbc516cd04b7fe
Mohamed122-r <anskajamal@gmail.com>
2025-11-28 15:11:59 +0200

    Update api.js

--- frontend-nextjs/lib/api.js
+++ frontend-nextjs/lib/api.js
@@ -1,1 +1,20 @@
 
+import axios from 'axios';
+
+const API_BASE_URL = 'https://mohamedalamin.wuaze.com/api';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const productAPI = {
+  getProducts: () => api.get('/products'),
+  getProduct: (slug) => api.get(`/products/${slug}`),
+  getFeaturedProducts: () => api.get('/products/featured'),
+  getCategories: () => api.get('/categories'),
+};
+
+export default api;
