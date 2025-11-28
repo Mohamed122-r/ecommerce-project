574cce977a3e366355a68b06970e1b58cb37128a
Mohamed122-r <anskajamal@gmail.com>
2025-11-28 15:11:23 +0200

    Update next.config.js

--- frontend-nextjs/next.config.js
+++ frontend-nextjs/next.config.js
@@ -1,1 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['mohamedalamin.wuaze.com'],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: 'https://mohamedalamin.wuaze.com/api',
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://mohamedalamin.wuaze.com/api/:path*',
+      },
+    ];
+  },
+}
 
+module.exports = nextConfig
